480d321079db4b80ee7b7a1f8082750526db3503
Rybailo <rybailo.orest4@gmail.com>
2022-09-23 01:07:31 +0300

    done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryJs = document.querySelector(".gallery");
+
+const galleryItem = galleryItems
+  .map(
+    (item) => `
+    <div class="gallery__item">
+      <a class="gallery__link" href="${item.original}">
+        <img
+          class="gallery__image"
+          src="${item.preview}"
+          data-source="${item.original}"
+          alt="${item.description}"
+        />
+      </a>
+    </div>`
+  )
+  .join("");
+
+galleryJs.innerHTML = galleryItem;
+galleryJs.addEventListener("click", onImgClick);
+
+function onImgClick(evt) {
+  evt.preventDefault();
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+    <img src='${evt.target.dataset.source}' width="800" height="600">
+`);
+
+  instance.show();
+
+  galleryJs.addEventListener("keydown", (evt) => {
+    if (evt.code === "Escape") {
+      instance.close();
+    }
+  });
+}
